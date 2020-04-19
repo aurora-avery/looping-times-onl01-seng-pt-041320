5143d72198583328abc499742465c7f6a7eb17ba
Aurora Avery <aurora.avery@yahoo.com>
2020-04-19 17:31:42 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -4,11 +4,12 @@ def using_times
   7.times do
     puts "Wingardium Leviosa"
     spell_cast = spell_cast - 1 
+    puts "#{spell_cast} tries left or I leave it for Hermione!"
 end
 
 
 
-puts "#{spell_cast} tries left or I leave it for Hermione!"
+
 
 
 
