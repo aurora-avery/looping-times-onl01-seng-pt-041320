1421808ca9c792d5fde7b2a5dd53eff87163ce38
Aurora Avery <aurora.avery@yahoo.com>
2020-04-19 17:17:36 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,6 +1,12 @@
-def using_times
-  
+def using_times = 25
+  7.times do
+    puts "Wingardium Leviosa"
+    using_times = using_times - 1 
 end
+
+
+
+puts "#{using_times} tries left before I leave it for Hermonie"
 
 
 
